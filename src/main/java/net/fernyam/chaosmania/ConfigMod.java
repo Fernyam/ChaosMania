a34572fe9c07ccd67cc807ca4356bb294fa835d3
Fernyam_ <sasaafonastev@gmail.com>
2026-05-11 22:53:44 +0300

    Comment out

--- src/main/java/net/fernyam/chaosmania/ConfigMod.java
+++ src/main/java/net/fernyam/chaosmania/ConfigMod.java
@@ -2,11 +2,19 @@ package net.fernyam.chaosmania;
 
 import net.neoforged.neoforge.common.ModConfigSpec;
 
+import java.util.List;
+
 public class ConfigMod {
 
 
     public static final ModConfigSpec.BooleanValue DISABLE_ANIMAL_BREEDING;
-
+    public static final ModConfigSpec.BooleanValue DISABLE_SEED_PLANTING;
+    public static final ModConfigSpec.BooleanValue DISABLE_VILLAGER_TRADING;
+    public static final ModConfigSpec.BooleanValue DISABLE_PICKUP_ITEM;
+    public static final ModConfigSpec.BooleanValue DISABLE_WANDER_TRADING;
+    public static final ModConfigSpec.BooleanValue DISABLE_BREAK_BLOCK;
+    public static final ModConfigSpec.BooleanValue DISABLE_PLACE_BLOCK;
+    public static final ModConfigSpec.BooleanValue DISABLE_ITEM_DROPS;
 
 
 
@@ -23,6 +31,33 @@ public class ConfigMod {
                 .comment("Запретить размножение животных")
                 .define("disableAnimalBreeding", true);
 
+        DISABLE_SEED_PLANTING = BUILDER
+                .comment("Запретить посадку семян")
+                .define("disableSeedPlanting", true);
+
+        DISABLE_VILLAGER_TRADING = BUILDER
+                .comment("Запретить торговлю с жителями")
+                .define("disableVillagerTrading", true);
+
+        DISABLE_WANDER_TRADING = BUILDER
+                .comment("Запретить торговлю с торговцами")
+                .define("disableWanderTrading", true);
+
+        DISABLE_PICKUP_ITEM = BUILDER
+                .comment("Запретить подбирать предметы")
+                        .define("disablePickupItem" , true);
+
+        DISABLE_ITEM_DROPS = BUILDER
+                .comment("Запретить выбрасывать предметы")
+                .define("disableItemDrops" , true);
+
+        DISABLE_BREAK_BLOCK = BUILDER
+                .comment("Запретить ломать блок")
+                .define("disablePickupItem" , true);
+
+        DISABLE_PLACE_BLOCK = BUILDER
+                .comment("Запретить устанавливать блок")
+                .define("disablePickupItem" , true);
 
 
         BUILDER.pop();
